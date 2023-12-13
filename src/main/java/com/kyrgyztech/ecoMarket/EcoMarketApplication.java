97bc956c968a09c03380863a3994ec6f53237f04
zhanybekovalmaz <zhanybekovalmaz23@gmail.com>
2023-12-14 00:54:56 +0600

    App started, connected to Postgresql

---
+++ src/main/java/com/kyrgyztech/ecoMarket/EcoMarketApplication.java
@@ -0,0 +1,13 @@
+package com.kyrgyztech.ecoMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kyrgyztech/ecoMarket/EcoMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyrgyztech.ecoMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
