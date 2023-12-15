d0a10345bb63a9a6ed802c9fdd5e8c36719830b0
zhanybekovalmaz <zhanybekovalmaz23@gmail.com>
2023-12-15 18:49:47 +0600

    initial commit

--- src/main/java/com/kyrgyztech/ecoMarket/entity/OrderHistory.java
+++ src/main/java/com/kyrgyztech/ecoMarket/entity/OrderHistory.java
@@ -1,4 +1,29 @@
 package com.kyrgyztech.ecoMarket.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "orders")
 public class OrderHistory {
+
+    @SequenceGenerator(
+            name = "order_sequence",
+            sequenceName = "order_sequence",
+            allocationSize = 1
+    )
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "order_sequence"
+    )
+    private Long id;
+
 }
